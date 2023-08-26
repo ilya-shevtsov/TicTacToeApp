f49fde461f0be9916ca6c663b53f6bf038e0af04
ilya-shevtsov <Dayzilya@gmail.com>
2023-08-26 05:38:59 +0400

    App is working! (it took some time...) (Initial commit)

---
+++ app/src/main/java/com/tictacktoeapp/data/RealtimeMessagingClient.kt
@@ -0,0 +1,10 @@
+package com.tictacktoeapp.data
+
+import kotlinx.coroutines.flow.Flow
+
+interface RealtimeMessagingClient {
+    fun getGameStateStream(): Flow<GameState>
+    suspend fun sendAction(action: MakeTurn)
+    suspend fun close()
+
+}
---
+++ app/src/main/java/com/tictacktoeapp/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.tictacktoeapp.di
+
+import com.tictacktoeapp.data.KtorRealtimeMessagingClient
+import com.tictacktoeapp.data.RealtimeMessagingClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.features.logging.*
+import io.ktor.client.features.websocket.*
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpClient(): HttpClient {
+        return HttpClient(CIO) {
+            install(Logging)
+            install(WebSockets)
+        }
+    }
+
+    @Singleton
+    @Provides
+    fun provideRealtimeMessagingClient(httpClient: HttpClient): RealtimeMessagingClient {
+        return KtorRealtimeMessagingClient(httpClient)
+    }
+
+}
